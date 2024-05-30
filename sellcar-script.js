c56df687a6e2a4620ab23b2bfcd129118389ceeb
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-05-30 22:52:12 +0700

    fixed bug

--- sellcar-script.js
+++ sellcar-script.js
@@ -15,11 +15,12 @@ document
       };
 
       reader.readAsDataURL(file);
-    } else {
-      preview.src = "";
-      preview.style.display = "none";
-      defaultImage.style.display = "block";
-    }
+    } 
+    // else {
+    //   preview.src = "";
+    //   preview.style.display = "none";
+    //   defaultImage.style.display = "block";
+    // }
   });
 
 function myFunction() {
